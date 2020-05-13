b50d518b597eb7312d21327d54d8098161e73697
GitHub-star-arch <nithinraggov@gmail.com>
2020-05-13 08:26:56 -0400

    poof

--- Log.js
+++ Log.js
@@ -3,5 +3,18 @@ class Log extends BaseClass{
     super(x,y,20,height,angle);
     this.image = loadImage("sprites/wood2.png");
     Matter.Body.setAngle(this.body, angle);
+    this.vanish = 255;
   }
+  display(){
+     if(this.body.speed<5){
+       super.display();
+     } else {
+       World.remove(world,this.body)
+      push();
+       this.vanish=this.vanish-5;
+       tint(255,this.vanish)
+       image(this.image,this.body.position.x,this.body.position.y,50,50);
+       pop();
+     }
+    }
 }
